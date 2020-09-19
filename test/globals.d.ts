1a583390a2e93a3566c1b7cf75c4e12895072414
Mário Vilela <mariolf.vilela@gmail.com>
2020-09-19 09:26:20 -0300

    add test with jest, functional's test

--- test/globals.d.ts
+++ test/globals.d.ts
@@ -1,3 +1,6 @@
 declare namespace NodeJS {
-  interface Global {}
+  interface Global {
+    //https://stackoverflow.com/a/51114250
+    testRequest: import("supertest").SuperTest<import("supertest").Test>;
+  }
 }
